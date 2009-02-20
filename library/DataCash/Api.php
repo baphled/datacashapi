4b9ca495f9afeb6f1727314addf5a89a3e34c1c3
Yomi (baphled) Colledge <baphled@boodah.net>
2009-02-20 09:17:27 +0000

    General cleanup & refactoring of class. Is now a little cleaner.

--- library/DataCash/Api.php
+++ library/DataCash/Api.php
@@ -32,6 +32,56 @@ class DataCash_Api {
 		$this->_config = Zend_Registry::get($config->environment)->datacash;
 	}
 	
+	private function _validateCard($params) {
+	if (!array_key_exists('Card',$params)) {
+			throw new Zend_Exception('No card data.');
+		}
+		$cardDataArray = $params['Card'];
+		if (empty($cardDataArray) ||
+			 !array_key_exists('pan',$cardDataArray) ||
+			 !array_key_exists('expirydate',$cardDataArray)) {
+			throw new Zend_Exception('Need to pass array containing cards details');
+		}
+	}
+	/**
+	 * Checks to see if we need to do a CV2 check, if so validates our params.
+	 *
+	 * @param array $params
+	 * @return string	$xml or false if not doing cv2 checks.
+	 * 
+	 */
+	private function _validateCV2Avs($params = array()) {
+		if (empty($params) || !isset($this->_config->cv2avs->check)) {
+			throw new Zend_Exception('No datacash cv2avs settings, please resolve.');
+		} 
+		if (true === $this->_config->cv2avs->check && !array_key_exists('CV2Avs', $params)) {
+			throw new Zend_Exception('CV2 data not present');
+		}
+		return $this->_handleCV2Address($params['CV2Avs']);
+	}
+
+	/**
+	 * Validates our requests parameters.
+	 *
+	 * @param array $dataArray
+	 */
+	private function _validateRequest($dataArray) {
+		if (!isset($this->_config->extendedPolicy->set)) {
+			throw new Zend_Exception('Must have extended policy setting in config file.');
+		}
+		if (empty($dataArray)) {
+			throw new Zend_Exception('Parameters must be in array format.');
+		}
+		
+		if (!array_key_exists('Card',$dataArray)) {
+			throw new Zend_Exception('Must have card details.');
+		}
+		
+		if (!array_key_exists('Transaction',$dataArray)) {
+			throw new Zend_Exception('Must have transaction details.');
+		}
+	}
+
 	/**
 	 * sets up and gets our authentication information from our
 	 * environments configuration file.
@@ -42,7 +92,7 @@ class DataCash_Api {
 	 * @param 	String 	$type	The type of request we are about to make.
 	 * @return 	String	$xml	Our resulting XML element
 	 */
-	private function _setAuth($type = 'deposit') {
+	private function _handleAuth($type = 'deposit') {
 		$xml = xmlwriter_open_memory();
 		xmlwriter_start_element($xml, 'Authentication');
 		xmlwriter_write_element($xml, 'client', $this->_config->$type->client);
@@ -59,17 +109,10 @@ class DataCash_Api {
 	 * @param 	Array 	$cardDataArray
 	 * @return 	String	XML element.
 	 */
-	private function _setCardData($params = array()) {
-		if (!array_key_exists('Card',$params)) {
-			throw new Zend_Exception('No card data.');
-		}
+	private function _handleCardData($params = array()) {
+		$this->_validateCard($params);
 		
 		$cardDataArray = $params['Card'];
-		if (empty($cardDataArray) ||
-			 !array_key_exists('pan',$cardDataArray) ||
-			 !array_key_exists('expirydate',$cardDataArray)) {
-			throw new Zend_Exception('Need to pass array containing cards details');
-		}
 		
 		$xml = xmlwriter_open_memory();
 		xmlwriter_start_element($xml, 'Card');
@@ -82,42 +125,49 @@ class DataCash_Api {
 		if (!array_key_exists('CV2Avs',$params)) {
 			throw new Zend_Exception('No datacash cv2avs settings, please resolve.');
 		} 
-		xmlwriter_write_raw($xml,$this->_cv2avsCheck($params));
+		xmlwriter_write_raw($xml,$this->_validateCV2Avs($params));
 		xmlwriter_end_element($xml);
 
 		return xmlwriter_output_memory($xml, true);
 	}
 	
 	/**
-	 * Checks to determine whther we need to do a Cv2Av2 check.
+	 * Sets out TxnDetails
 	 *
-	 * @param unknown_type $params
-	 * @return string	$xml or false if not doing cv2 checks.
-	 * 
+	 * @param Array	 	$params Parameterrs need to create element
+	 * @return String	$xml	Resulting TxnDetails element in XML format.
 	 */
-	function _cv2avsCheck($params = array()) {
-		if (empty($params) || !isset($this->_config->cv2avs->check)) {
-			throw new Zend_Exception('No datacash cv2avs settings, please resolve.');
-		} 
-		if (true === $this->_config->cv2avs->check && !array_key_exists('CV2Avs', $params)) {
-			throw new Zend_Exception('CV2 data not present');
+	private function _handleTxnDetails($params = array()) {
+		if (empty($params)) {
+			throw new Zend_Exception('Parameters must be set');
 		}
-		return $this->_setCV2Address($params['CV2Avs']);
+		if (!array_key_exists('merchantreference',$params) || !array_key_exists('amount',$params)) {
+			throw new Zend_Exception('Must supply merchant reference & amount');
+		}
+		$xml = xmlwriter_open_memory();
+		xmlwriter_start_element($xml,'TxnDetails');
+		
+		xmlwriter_write_element($xml,'merchantreference',$params['merchantreference']);
+		xmlwriter_start_element($xml,'amount');
+		if (array_key_exists('currency',$params)) {
+			xmlwriter_write_attribute($xml,'currency',$params['currency']);
+		} else {
+			xmlwriter_write_attribute($xml,'currency','GBP');
+		}
+		xmlwriter_text($xml,$params['amount']);
+		xmlwriter_end_element($xml);
+		xmlwriter_end_element($xml);
+		return xmlwriter_output_memory($xml,true);
 	}
 	
 	/**
-	 * Sets our CV2Avs check inforamtion ready to send off to DataCash
+	 * Handles our CV2 Address information
 	 *
+	 * @param string $xml
 	 * @param array $params
-	 * @return string	$xml	Our resulting request body for Cv2Avs element.
-	 * 
+	 * @return string
 	 */
-	private function _setCV2Address($params) {
-		if (empty($params)) {
-			throw new Zend_Exception('no Address details.');
-		}
-		$xml = xmlwriter_open_memory();
-		xmlwriter_start_element($xml,'CV2Avs');
+	private function _handleAddress($xml, $params) {
 		if (array_key_exists('street_address1',$params)) {
 			xmlwriter_write_element($xml,'street_address1',$params['street_address1']);
 		}
@@ -133,6 +183,23 @@ class DataCash_Api {
 		} elseif (array_key_exists('street_address3',$params)) {
 			xmlwriter_write_element($xml,'street_address3',$params['street_address3']);
 		}
+		return $xml;
+	}
+	
+	/**
+	 * Sets our CV2Avs check inforamtion ready to send off to DataCash
+	 *
+	 * @param array $params
+	 * @return string	$xml	Our resulting request body for Cv2Avs element.
+	 * 
+	 */
+	private function _handleCV2Address($params) {
+		if (empty($params)) {
+			throw new Zend_Exception('no Address details.');
+		}
+		$xml = xmlwriter_open_memory();
+		xmlwriter_start_element($xml,'CV2Avs');
+		$xml = $this->_handleAddress($xml, $params);
 		if (array_key_exists('postcode',$params)) {
 			xmlwriter_write_element($xml,'postcode',$params['postcode']);
 		}
@@ -150,9 +217,9 @@ class DataCash_Api {
 	 * @param 	Array 	$params	Array storing data needed to make transaction.
 	 * @return 	String	$xml	Our CardTxn XML element.
 	 */
-	private function _setCardTxn($params = array()) {
+	private function _handleCardTxn($params = array()) {
 		$cardInfo = $params['Card'];
-		$card = $this->_setCardData($params);
+		$card = $this->_handleCardData($params);
 		if (!array_key_exists('method',$cardInfo)) {
 			throw new Zend_Exception('Must supply a transaction method');
 		}
@@ -166,6 +233,60 @@ class DataCash_Api {
 		xmlwriter_end_element($xml);
 
 		return xmlwriter_output_memory($xml, true);
+	}
+	
+	/**
+	 * Checks that we have all the needed extended policy data, 
+	 * throws exception if something goes wrong.
+	 *
+	 * @return bool
+	 */
+	private function _handleExtendedPolicy() {
+		if (false === $this->_checkPolicy('cv2_policy')) {
+			throw new Zend_Exception('Unable to set cv2_policy, all cv2 policy settings should be accessible.');
+		}
+		if (false === $this->_checkPolicy('postcode_policy')) {
+			throw new Zend_Exception('Unable to set postcode_policy, all policy postcode policy settings should be accessible.');
+		}
+		if (false === $this->_checkPolicy('address_policy')) {
+			throw new Zend_Exception('Unable to set address_policy, all policy address policy settings should be accessible.');
+		}
+		$xml = xmlwriter_open_memory();
+		xmlwriter_start_element($xml,'ExtendedPolicy');
+		xmlwriter_write_raw($xml, $this->_writePolicy('cv2_policy'));
+		xmlwriter_write_raw($xml, $this->_writePolicy('postcode_policy'));
+		xmlwriter_write_raw($xml, $this->_writePolicy('address_policy'));
+		xmlwriter_end_element($xml);
+		return xmlwriter_output_memory($xml,true);
+	}
+	
+	/**
+	 * Creates our 3DSecure XML elements, need to do 3DSecure checks
+	 * using DataCash
+	 *
+	 * @return String	$xml	3DSecure XML result.
+	 */
+	private function _handleThreeDSecure() {
+		if(!isset($this->_config->threeDSecure->verify)) {
+			throw new Zend_Exception('Need to set 3dSecure property.');
+		}
+		$xml = xmlwriter_open_memory();
+		xmlwriter_start_element($xml,'ThreeDSecure');
+		if(1 == $this->_config->threeDSecure->verify) {
+			xmlwriter_write_element($xml,'verify','yes');
+			xmlwriter_write_element($xml,'merchant_url',$this->_config->threeDSecure->merchant_url);
+			xmlwriter_write_element($xml,'purchase_desc',$this->_config->threeDSecure->purchase_desc);
+			xmlwriter_write_element($xml,'purchase_datetime',date('Ymd H:i:s'));
+			xmlwriter_start_element($xml,'Browser');
+			xmlwriter_write_element($xml,'device_category',$this->_config->threeDSecure->device_category);
+			xmlwriter_write_element($xml,'accept_headers',$this->_config->threeDSecure->accept_headers);
+			xmlwriter_write_element($xml,'user_agent',$_SERVER['HTTP_USER_AGENT']);
+			xmlwriter_end_element($xml);
+		} else {
+			xmlwriter_write_element($xml,'verify','no');
+		}
+		xmlwriter_end_element($xml);
+		return xmlwriter_output_memory($xml,true);
 	}
 	
 	/**
@@ -239,84 +360,24 @@ class DataCash_Api {
 		xmlwriter_end_element($xml);
 		return xmlwriter_output_memory($xml,true);
 	}
+
 	/**
-	 * Checxks that we have all the needed extended policy data, throws exception if something goes wrong.
+	 * Writes our XML request which we need to send to DataCash
 	 *
-	 * @return bool
+	 * @param string $auth
+	 * @param string $cardTxn
+	 * @param string $txnDetails
+	 * @return string
 	 */
-	private function _handleExtendedPolicy() {
-		if (false === $this->_checkPolicy('cv2_policy')) {
-			throw new Zend_Exception('Unable to set cv2_policy, all cv2 policy settings should be accessible.');
-		}
-		if (false === $this->_checkPolicy('postcode_policy')) {
-			throw new Zend_Exception('Unable to set postcode_policy, all policy postcode policy settings should be accessible.');
-		}
-		if (false === $this->_checkPolicy('address_policy')) {
-			throw new Zend_Exception('Unable to set address_policy, all policy address policy settings should be accessible.');
-		}
+	private function _writeRequest( $auth, $cardTxn, $txnDetails) {
 		$xml = xmlwriter_open_memory();
-		xmlwriter_start_element($xml,'ExtendedPolicy');
-		xmlwriter_write_raw($xml, $this->_writePolicy('cv2_policy'));
-		xmlwriter_write_raw($xml, $this->_writePolicy('postcode_policy'));
-		xmlwriter_write_raw($xml, $this->_writePolicy('address_policy'));
+		xmlwriter_start_element($xml, 'Request');
+		xmlwriter_write_raw($xml, $auth);
+		xmlwriter_start_element($xml, 'Transaction');
+		xmlwriter_write_raw($xml, $cardTxn);
+		xmlwriter_write_raw($xml, $txnDetails);
+		xmlwriter_write_raw($xml, $this->_handleThreeDSecure());
 		xmlwriter_end_element($xml);
-		return xmlwriter_output_memory($xml,true);
-	}
-	
-	/**
-	 * Sets out TxnDetails
-	 *
-	 * @param Array	 	$params Parameterrs need to create element
-	 * @return String	$xml	Resulting TxnDetails element in XML format.
-	 */
-	private function _setTxnDetails($params = array()) {
-		if (empty($params)) {
-			throw new Zend_Exception('Parameters must be set');
-		}
-		if (!array_key_exists('merchantreference',$params) || !array_key_exists('amount',$params)) {
-			throw new Zend_Exception('Must supply merchant reference & amount');
-		}
-		$xml = xmlwriter_open_memory();
-		xmlwriter_start_element($xml,'TxnDetails');
-		
-		xmlwriter_write_element($xml,'merchantreference',$params['merchantreference']);
-		xmlwriter_start_element($xml,'amount');
-		if (array_key_exists('currency',$params)) {
-			xmlwriter_write_attribute($xml,'currency',$params['currency']);
-		} else {
-			xmlwriter_write_attribute($xml,'currency','GBP');
-		}
-		xmlwriter_text($xml,$params['amount']);
-		xmlwriter_end_element($xml);
-		xmlwriter_end_element($xml);
-		return xmlwriter_output_memory($xml,true);
-	}
-	
-	/**
-	 * Creates our 3DSecure XML elements, need to do 3DSecure checks
-	 * using DataCash
-	 *
-	 * @return String	$xml	3DSecure XML result.
-	 */
-	private function _threeDSecure() {
-		if(!isset($this->_config->threeDSecure->verify)) {
-			throw new Zend_Exception('Need to set 3dSecure property.');
-		}
-		$xml = xmlwriter_open_memory();
-		xmlwriter_start_element($xml,'ThreeDSecure');
-		if(1 == $this->_config->threeDSecure->verify) {
-			xmlwriter_write_element($xml,'verify','yes');
-			xmlwriter_write_element($xml,'merchant_url',$this->_config->threeDSecure->merchant_url);
-			xmlwriter_write_element($xml,'purchase_desc',$this->_config->threeDSecure->purchase_desc);
-			xmlwriter_write_element($xml,'purchase_datetime',date('Ymd H:i:s'));
-			xmlwriter_start_element($xml,'Browser');
-			xmlwriter_write_element($xml,'device_category',$this->_config->threeDSecure->device_category);
-			xmlwriter_write_element($xml,'accept_headers',$this->_config->threeDSecure->accept_headers);
-			xmlwriter_write_element($xml,'user_agent',$_SERVER['HTTP_USER_AGENT']);
-			xmlwriter_end_element($xml);
-		} else {
-			xmlwriter_write_element($xml,'verify','no');
-		}
 		xmlwriter_end_element($xml);
 		return xmlwriter_output_memory($xml,true);
 	}
@@ -328,30 +389,10 @@ class DataCash_Api {
 	 * @return 	String		$xml		The XML request we want to send to DataCash
 	 */
 	function setRequest($dataArray = array(), $method='deposit') {
-		if (!isset($this->_config->extendedPolicy->set)) {
-			throw new Zend_Exception('Must have extended policy setting in config file.');
-		}
-		if (empty($dataArray)) {
-			throw new Zend_Exception('Parameters must be in array format.');
-		}
-		$auth = $this->_setAuth($method);
-		if (!array_key_exists('Card',$dataArray)) {
-			throw new Zend_Exception('Must have card details.');
-		}
-		$cardTxn = $this->_setCardTxn($dataArray);
-		if (!array_key_exists('Transaction',$dataArray)) {
-			throw new Zend_Exception('Must have transaction details.');
-		}
-		$txnDetails = $this->_setTxnDetails($dataArray['Transaction']);
-		$xml = xmlwriter_open_memory();
-		xmlwriter_start_element($xml,'Request');
-		xmlwriter_write_raw($xml,$auth);
-		xmlwriter_start_element($xml,'Transaction');
-		xmlwriter_write_raw($xml,$cardTxn);
-		xmlwriter_write_raw($xml,$txnDetails);
-		xmlwriter_write_raw($xml,$this->_threeDSecure());
-		xmlwriter_end_element($xml);
-		xmlwriter_end_element($xml);
-		return xmlwriter_output_memory($xml,true);
+		$this->_validateRequest($dataArray, $method);
+		$auth = $this->_handleAuth($method);
+		$cardTxn = $this->_handleCardTxn($dataArray);
+		$txnDetails = $this->_handleTxnDetails($dataArray['Transaction']);
+		return $this->_writeRequest($auth, $cardTxn, $txnDetails);
 	}
 }
